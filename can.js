996a64b9ce2c257cc8c26d8760335c1c3b27879b
Lorcan <lorcan.james@yahoo.com>
2021-05-01 15:34:03 +0100

    tidy up can.js

--- can.js
+++ can.js
@@ -1,13 +1,7 @@
 import Grid from './main/Structure/Grid.js';
-// import {BinaryTree} from "./main/Maze/BinaryTree.js";
 import Draw from './main/Structure/Draw.js';
 import BinaryTree from './main/Maze/BinaryTree.js';
 
-
-
-
-// need to correct the binary tree and the links because it is not doing it correctly
-// as there is a cell which is not linked which shouldn't be possible
 document.addEventListener('DOMContentLoaded', () => {
     
     var button = document.querySelector('#create_maze');
@@ -24,35 +18,5 @@ document.addEventListener('DOMContentLoaded', () => {
         Draw.draw_maze(g, canvas);
     }
 
-    
-    
-
-    
-
 });
-
-
-
-
-// ctx.fillStyle='black';
-// ctx.fillRect(20, 20, 150, 10);  
-
-//need to implement the maze creator before the draw thing is called!!!!!!
-
-
-
-//d.draw_maze();
-
-
-// canvas.width = window.innerWidth;
-// canvas.height = window.innerHeight;
-// canvas.innerHTML = "Hello";
-
-// var grid = new Grid(100, 100);
-
-// var d = new Draw(grid)
-
-// d.draw_maze();
-
-// var thing = document.querySelector("#mycanvas");
     
