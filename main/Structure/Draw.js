97092f685d837ab970557354c3f76d8c90cc9339
Lorcan <lorcan.james@yahoo.com>
2021-05-03 14:56:51 +0100

    update value positions

--- main/Structure/Draw.js
+++ main/Structure/Draw.js
@@ -88,12 +88,11 @@ import Cell from './Cell.js';
     }
 
     static display_distance(grid, canvas) {
-        // change this to the commented part to see if it prints out the cell value
         var cell_size = canvas.width / grid.columns;
         window.ctx.font = "20px Georgia";
         for (var cell of grid.each_cell()) {
-            var x = (cell.column * cell_size) + (cell_size / 2);
-            var y = (cell.row * cell_size) + (cell_size / 2);
+            var x = (cell.column * cell_size) + (cell_size / 2) - 5;
+            var y = (cell.row * cell_size) + (cell_size / 2) + 1;
             window.ctx.fillText(cell.value, x, y);
         }
     }
